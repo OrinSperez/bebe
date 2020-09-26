20803a885e62652596c2104122c82e594d07e309
Orlin <orlinsperez28@gmail.com>
2020-09-25 21:12:46 -0400

    Updates

---
+++ precache-manifest.a6f8edf8974a2fc17cd3e3adfd32db94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1961a2f1d058d4bd4d0ffcb1ce46cb32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bda9083622a44bf3b35a",
+    "url": "/static/css/main.7be7a287.chunk.css"
+  },
+  {
+    "revision": "d2c613693d306737ce56",
+    "url": "/static/js/2.0f63fdbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0f63fdbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda9083622a44bf3b35a",
+    "url": "/static/js/main.7a2e7f92.chunk.js"
+  },
+  {
+    "revision": "d6183457ec3ec037fb98",
+    "url": "/static/js/runtime-main.864a714f.js"
+  }
+]);
