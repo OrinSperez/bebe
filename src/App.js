1d0aa3913b6c9f2fc5c3c8efabb4766fb19886b1
Orlin <orlinsperez28@gmail.com>
2020-09-25 20:24:19 -0400

    0.01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./style/style.scss";
+import Tab from "./componentes/tab";
+import Card from "./componentes/Card";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Tab />
+
+    
+    <div background="blue" className="ed-video">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/wKcnM1L-u18" 
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+    autoplay="1"
+    controls="1"
+    color="blue"
+    allowfullscreen></iframe>
     </div>
+
+   
+
+    </>
   );
 }
 
---
+++ src/componentes/tab.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../style/style.scss";
+
+const Tab = ()=>(
+    <>
+    <header class="ed-header s-bg-grey s-py-2">
+  <div class="ed-grid lg-grid-5">
+  
+    <div class="s-cross-center s-main-center lg-main-start">
+        <img alt="Ya no hay foto"  width="200"
+          height="200"src="https://cdn.pixabay.com/photo/2017/01/17/08/44/valentines-day-1986217__340.png"/>
+    </div>
+    <nav class="nav lg-cols-4 s-cross-center ed-grid full">
+    
+      <ul class="menu s-main-distribute lg-to-right s-mb-0 s-pl-0 s-py-1">
+   
+     <h1 className="fondo">Te amo mucho mi amor</h1>
+    
+       
+      </ul>
+    </nav>
+  </div>
+</header>
+
+    </>
+)
+
+export default Tab;
